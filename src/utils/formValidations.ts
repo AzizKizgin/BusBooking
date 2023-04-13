bb046cbbbb61d2186a1942fe6a9bd0ea1025655e
Aziz Kizgin <azizkzgn@gmail.com>
2023-04-13 21:19:19 +0300

    feat: RegisterSchema created

--- src/utils/formValidations.ts
+++ src/utils/formValidations.ts
@@ -1,7 +1,6 @@
 /** @format */
 
 import * as Yup from "yup";
-// import {checkIfEmailIsTaken, checkIfUsernameIsTaken} from '../data/data';
 
 export const LoginSchema = (strings: any) =>
   Yup.object().shape({
@@ -13,36 +12,34 @@ export const LoginSchema = (strings: any) =>
       .required(strings.passwordIsRequired),
   });
 
-// export const SignUpSchema = (strings: any) =>
-//   Yup.object().shape({
-//     email: Yup.string()
-//       .email(strings.emailIsInvalid)
-//       .required(strings.emailIsRequired)
-//       .test('is-email-taken', strings.emailIsInUse, async (value) => {
-//         if (value) {
-//           const isTaken = await checkIfEmailIsTaken(value);
-//           return !isTaken;
-//         }
-//         return true;
-//       }),
-//     password: Yup.string()
-//       .min(6, strings.passwordNeedsToBe6CharactersOrMore)
-//       .required(strings.passwordIsRequired),
-//     confirmPassword: Yup.string()
-//       .oneOf([Yup.ref('password'), undefined], strings.passwordDoesNotMatch)
-//       .required(strings.passwordIsRequired),
-//     username: Yup.string()
-//       .min(3, strings.usernameNeedsToBe3CharactersOrMore)
-//       .required(strings.usernameIsRequired)
-//       .test('is-username-taken', strings.usernameIsTaken, async (value) => {
-//         if (value) {
-//           const isTaken = await checkIfUsernameIsTaken(value);
-//           return !isTaken;
-//         }
-//         return true;
-//       }),
-//     agreed: Yup.boolean().oneOf([true], strings.shouldAgreeToTerms),
-//   });
+export const RegisterSchema = (strings: any) =>
+  Yup.object().shape({
+    email: Yup.string()
+      .email(strings.emailIsInvalid)
+      .required(strings.emailIsRequired),
+    password: Yup.string()
+      .min(6, strings.passwordNeedsToBe6CharactersOrMore)
+      .required(strings.passwordIsRequired),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref("password"), undefined], strings.passwordDoesNotMatch)
+      .required(strings.passwordIsRequired),
+    name: Yup.string()
+      .min(3, strings.nameNeedsToBe3CharactersOrMore)
+      .required(strings.nameisrequired),
+    surname: Yup.string()
+      .required(strings.surnameisrequired)
+      .min(2, strings.surnameNeedsToBe2CharactersOrMore),
+    agreed: Yup.boolean().oneOf([true], strings.shouldAgreeToTerms),
+    identityNumber: Yup.string()
+      .required(strings.identitynumberisrequired)
+      .test(
+        "len",
+        strings.identitynumberneedsToBe11Characters,
+        (val: any) => val && val.length === 11,
+      ),
+    birthDate: Yup.string().required(strings.birthdateisrequired),
+    gender: Yup.string().required(strings.genderisrequired),
+  });
 
 export const ForgotPasswordSchema = (strings: any) =>
   Yup.object().shape({
