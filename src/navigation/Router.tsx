b2b4ad07717163aaab544a4746a7bf3dcf660e7c
Aziz Kizgin <azizkzgn@gmail.com>
2023-04-13 17:03:51 +0300

    refactor: import updated

--- src/navigation/Router.tsx
+++ src/navigation/Router.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import AuthenticationNav from "./AuthenticationNav";
 import MainNav from "./MainNav";
 import { NavigationContainer } from "@react-navigation/native";
-import { useAuth } from "../context/AuthContex";
+import { useAuth } from "../context/AuthContext";
 
 const Router = () => {
   const { user } = useAuth();
