40d92d45b2488ef3736b19613877af6cfb94ed1c
Aziz Kizgin <azizkzgn@gmail.com>
2023-04-13 21:21:57 +0300

    feat: navigation updated

--- src/navigation/AuthenticationNav.tsx
+++ src/navigation/AuthenticationNav.tsx
@@ -3,13 +3,26 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { ForgotPassword, Login, Register } from "../screens";
+import theme from "../theme/theme";
+import { useLocalization } from "../context/LocalizationContext";
 const AuthenticationNav = () => {
   const Stack = createNativeStackNavigator();
+  const { strings } = useLocalization();
   return (
     <Stack.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}
+      screenOptions={({ route }) => ({
+        headerShown: route.name === "Login" ? false : true,
+        animation: "slide_from_right",
+        headerStyle: {
+          backgroundColor: theme.colors.frenchBlue,
+        },
+        headerTintColor: theme.colors.coconut,
+        headerShadowVisible: false,
+        title:
+          (route.name === "Register" && strings.registerScreen) ||
+          strings.forgotPasswordScreen,
+      })}
+      initialRouteName='Login'
     >
       <Stack.Screen name='Login' component={Login} />
       <Stack.Screen name='Register' component={Register} />
