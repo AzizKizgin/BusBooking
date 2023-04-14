3c6ecf4693e2f81e0798ea4e1310d0adbb2011d7
Aziz Kizgin <azizkzgn@gmail.com>
2023-04-14 15:10:26 +0300

    feat: types added

--- src/types/type.d.ts
+++ src/types/type.d.ts
@@ -5,3 +5,29 @@ type AuthenticationParamsList = {
   Register: undefined;
   ForgotPassword: undefined;
 };
+
+type HomeParamsList = {
+  Home: undefined;
+  Voyages: VoyageSearchType;
+  VoyageDetail: undefined;
+  Payment: undefined;
+};
+
+type VoyageSearchType = {
+  origin: string;
+  destination?: string;
+  departureDate: string;
+  returnDate?: string;
+  hasReturn: boolean;
+};
+
+type VoyageType = {
+  id: number;
+  origin: string;
+  destination: string;
+  departureDate: string;
+  returnDate?: string;
+  price: number;
+  company: string;
+  hasReturn: boolean;
+};
