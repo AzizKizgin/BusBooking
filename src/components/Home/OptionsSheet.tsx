2035ff079f2f2c450cfba879bb4e8c39bd7af85c
Aziz Kizgin <azizkzgn@gmail.com>
2023-04-14 17:56:06 +0300

    feat: option bottom sheet created

---
+++ src/components/Home/OptionsSheet.tsx
@@ -0,0 +1,90 @@
+/** @format */
+
+import React, { FC } from "react";
+import {
+  Actionsheet,
+  Box,
+  HStack,
+  Pressable,
+  Text,
+  useDisclose,
+} from "native-base";
+import { useAuth } from "../../context/AuthContext";
+import { Ionicons } from "@expo/vector-icons";
+import theme from "../../theme/theme";
+
+interface AccountMenuButtonProps {
+  goOption: () => void;
+}
+
+const OptionsSheet: FC<AccountMenuButtonProps> = ({ goOption }) => {
+  const { isOpen, onOpen, onClose } = useDisclose();
+  const { logout } = useAuth();
+  return (
+    <>
+      <Pressable padding={2} onPress={onOpen}>
+        <Box>
+          <Ionicons
+            name='settings-outline'
+            size={24}
+            color={theme.colors.coconut}
+          />
+        </Box>
+      </Pressable>
+      <Actionsheet
+        isOpen={isOpen}
+        onClose={onClose}
+        _backdrop={{
+          backgroundColor: "rgba(0,0,0,0.0)",
+        }}
+      >
+        <Actionsheet.Content
+          backgroundColor={"frenchBlue"}
+          padding={0}
+          borderTopLeftRadius={15}
+          borderTopRightRadius={15}
+        >
+          <Actionsheet.Item
+            onPress={() => {
+              onClose();
+              goOption();
+            }}
+            backgroundColor={"frenchBlue"}
+          >
+            <HStack space={8} alignItems={"center"} paddingX={4}>
+              <Ionicons
+                name='settings-outline'
+                size={24}
+                color={theme.colors.coconut}
+              />
+              <Text color={"coconut"} fontSize={"md"}>
+                Settings
+              </Text>
+            </HStack>
+          </Actionsheet.Item>
+
+          <Actionsheet.Item
+            onPress={() => {
+              onClose();
+              logout();
+            }}
+            backgroundColor={"frenchBlue"}
+          >
+            <HStack space={8} alignItems={"center"} paddingX={4}>
+              <Ionicons
+                name='ios-exit-outline'
+                size={24}
+                color={theme.colors.coconut}
+              />
+              <Text color={"coconut"} fontSize={"md"}>
+                Log Out
+              </Text>
+            </HStack>
+          </Actionsheet.Item>
+        </Actionsheet.Content>
+      </Actionsheet>
+    </>
+  );
+};
+
+export default OptionsSheet;
