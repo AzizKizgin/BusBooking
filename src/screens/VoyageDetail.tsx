af21a2b0dc6d6fc20086ba073834ca4ac67c3c40
Aziz Kizgin <azizkzgn@gmail.com>
2023-04-14 22:12:58 +0300

    feat: bus seat selector added

--- src/screens/VoyageDetail.tsx
+++ src/screens/VoyageDetail.tsx
@@ -2,14 +2,35 @@
 
 import React from "react";
 import { Box, Center, HStack, ScrollView, Text, VStack } from "native-base";
-import { useRoute, RouteProp } from "@react-navigation/native";
+import { useRoute, RouteProp, useNavigation } from "@react-navigation/native";
 import VoyageItem from "../components/Voyages/VoyageItem";
 import BusSeatSelector from "../components/VoyageDetail/BusSeatSelector";
 import SelectInfo from "../components/VoyageDetail/SelectInfo";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { useLocalization } from "../context/LocalizationContext";
+import { useToast } from "@aziz_kizgin/react-native-toast-message";
 
 const VoyageDetail = () => {
   const route = useRoute<RouteProp<HomeParamsList, "VoyageDetail">>();
   const voyage = route.params;
+  const navigation = useNavigation<NativeStackNavigationProp<HomeParamsList>>();
+  const [selectedSeats, setSelectedSeats] = React.useState<number[]>([]);
+  const { strings } = useLocalization();
+  const { showToast } = useToast();
+  const goPayment = () => {
+    if (selectedSeats.length === 0) {
+      showToast({
+        color: "error",
+        message: strings.chooseAtLeast1Seat,
+      });
+    } else {
+      navigation.navigate("Payment", {
+        ...voyage,
+        selectedSeats,
+      });
+    }
+  };
+
   return (
     <Box flex={1} p={4}>
       <Box marginY={4}>
@@ -24,7 +45,12 @@ const VoyageDetail = () => {
         justifyContent='center'
       >
         <SelectInfo />
-        <BusSeatSelector />
+        <BusSeatSelector
+          totalPrice={selectedSeats.length * voyage.price}
+          selectedSeats={selectedSeats}
+          setSelectedSeats={setSelectedSeats}
+          goPayment={goPayment}
+        />
       </Box>
     </Box>
   );
