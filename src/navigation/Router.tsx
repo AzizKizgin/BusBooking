bb6ad06516caec9ec9172725afc849d697bb1566
Aziz Kizgin <azizkzgn@gmail.com>
2023-04-14 15:10:02 +0300

    feat: router updated

--- src/navigation/Router.tsx
+++ src/navigation/Router.tsx
@@ -1,16 +1,38 @@
 /** @format */
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AuthenticationNav from "./AuthenticationNav";
-import MainNav from "./MainNav";
+import HomeNav from "./HomeNav";
 import { NavigationContainer } from "@react-navigation/native";
 import { useAuth } from "../context/AuthContext";
+import { ActivityIndicator } from "react-native";
+import { Box, Image } from "native-base";
 
 const Router = () => {
   const { user } = useAuth();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (user) {
+      setLoading(false);
+    }
+  }, [user]);
+
+  if (loading) {
+    return (
+      <Box flex={1} justifyContent='center' alignItems='center'>
+        <Image
+          source={require("../assets/images/logo.png")}
+          alt='logo'
+          resizeMode='contain'
+        />
+      </Box>
+    );
+  }
+
   return (
     <NavigationContainer>
-      {user ? <MainNav /> : <AuthenticationNav />}
+      {user ? <HomeNav /> : <AuthenticationNav />}
     </NavigationContainer>
   );
 };
