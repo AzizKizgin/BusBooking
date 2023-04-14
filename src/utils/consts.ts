cae9f1cc09000421160251fa4c1d3b50f579908d
Aziz Kizgin <azizkzgn@gmail.com>
2023-04-14 15:10:41 +0300

    feat: fake data added

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,6 +1,99 @@
-import {Dimensions} from 'react-native';
+/** @format */
+
+import { Dimensions } from "react-native";
 
 export const screen = {
-  width: Dimensions.get('window').width,
-  height: Dimensions.get('window').height,
+  width: Dimensions.get("window").width,
+  height: Dimensions.get("window").height,
 };
+
+export const cities = [
+  "Istanbul",
+  "Ankara",
+  "Izmir",
+  "Bursa",
+  "Adana",
+  "Gaziantep",
+  "Konya",
+  "Antalya",
+  "Kayseri",
+];
+
+export const voyages = [
+  {
+    id: 1,
+    origin: "Istanbul",
+    destination: "Ankara",
+    departureDate: "21.04.2023",
+    price: 300,
+    company: "FastBus",
+    hasReturn: false,
+  },
+  {
+    id: 2,
+    origin: "Istanbul",
+    destination: "Ankara",
+    departureDate: "21.04.2023",
+    price: 200,
+    company: "BusTurk",
+    hasReturn: false,
+  },
+  {
+    id: 3,
+    origin: "Ankara",
+    destination: "Istanbul",
+    departureDate: "22.04.2023",
+    price: 500,
+    company: "FastBus",
+    hasReturn: false,
+  },
+  {
+    id: 4,
+    origin: "Ankara",
+    destination: "Istanbul",
+    departureDate: "22.04.2023",
+    price: 200,
+    company: "BusTurk",
+    hasReturn: false,
+  },
+  {
+    id: 5,
+    origin: "Istanbul",
+    destination: "Ankara",
+    departureDate: "23.04.2023",
+    returnDate: "24.04.2023",
+    price: 500,
+    company: "FastBus",
+    hasReturn: true,
+  },
+  {
+    id: 6,
+    origin: "Istanbul",
+    destination: "Ankara",
+    departureDate: "23.04.2023",
+    returnDate: "24.04.2023",
+    price: 400,
+    company: "BusTurk",
+    hasReturn: true,
+  },
+  {
+    id: 7,
+    origin: "Ankara",
+    destination: "Istanbul",
+    departureDate: "23.04.2023",
+    returnDate: "24.04.2023",
+    price: 400,
+    company: "FastBus",
+    hasReturn: true,
+  },
+  {
+    id: 8,
+    origin: "Ankara",
+    destination: "Istanbul",
+    departureDate: "23.04.2023",
+    returnDate: "24.04.2023",
+    price: 450,
+    company: "BusTurk",
+    hasReturn: true,
+  },
+];
